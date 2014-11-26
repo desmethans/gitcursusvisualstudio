021c00bb474504f4396565dfdbaabe01a8ec8993
desmethans <desmethans@hotmail.com>
2014-11-26 09:44:03 +0100

    Update Program.cs

--- GitCursusVisualStudio/Program.cs
+++ GitCursusVisualStudio/Program.cs
@@ -10,7 +10,7 @@ namespace GitCursusVisualStudio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo iedereen");
+            Console.WriteLine("Hallo allemaal");
         }
     }
 }
